a9aa41f4af4372076b6cb8dc6fab5c9dda6c1b38
MittyChan <245385830@qq.com>
2026-07-01 17:35:43 +0800

    建立项目

---
+++ back/src/test/java/com/cjd/back/BackApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cjd.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("Hello World");
+    }
+
+}
